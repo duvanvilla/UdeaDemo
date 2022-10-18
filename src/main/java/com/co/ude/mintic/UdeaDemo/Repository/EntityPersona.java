f26e5d5a3cc685645c00e9184c6b911da1f56ada
Duvan Villa <duvanvillaqd@gmail.com>
2022-10-17 22:41:47 -0500

    Actualizado al video #14

--- src/main/java/com/co/ude/mintic/UdeaDemo/Repository/EntityPersona.java
+++ src/main/java/com/co/ude/mintic/UdeaDemo/Repository/EntityPersona.java
@@ -4,21 +4,26 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-@Data
-@Entity
-@Table (name = "persona")
+@Data // Encapsulamiento: Anotación para encapsular los datos para así poder obtener y modificarlos (getters and setters)
+@Entity // Comportamiento: Anotación para definir que esta clase se comporte como una entity class
+@Table (name = "persona") // Definición: La clase va a ser una tabla
 public class EntityPersona {
-    @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
-    private Long id;
+    // Esta es la primera columna y se va a comportar como PrimaryKey
+    @Id // Esta es la anotación para definir esta columna como PK (PrimaryKey)
+    @GeneratedValue (strategy = GenerationType.AUTO) // Los valores de este campo se van a generar de manera automática
+    private Long id; // Para JPA no sirven los tipos de dato primitivo, sólo sirven los de tipo Object
 
+    // El resto de las columnas se definen con la anotación @Column
     @Column (name = "nombre")
     private String nombre;
 
     @Column (name = "apellido")
     private String apellido;
 
-    @Column (name = "documento")
+    @Column (name = "edad")
     private int edad;
+
+    @Column (name = "doc")
+    private String doc;
 
 }
